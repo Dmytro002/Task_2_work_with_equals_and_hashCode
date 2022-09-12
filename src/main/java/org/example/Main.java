f745f4cdccd60ad397d8eb871ebfbef611f3baab
Dmytro Svirniuk <dmytro123svi@gmail.com>
2022-09-12 21:32:52 +0300

    Error correction

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,8 +7,6 @@ import org.example.model.ContactNull;
 
 public class Main {
     public static void main(String[] args) {
-        ContactNull c = new ContactNull(1,"f",null);
-        ContactNull c1 = new ContactNull(1,"f","dsd");
-        System.out.println(c.equals(null));
+
     }
 }
