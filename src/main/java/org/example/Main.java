2488cce0607bf4ee4665ddbd6298bbe713baaa60
Dmytro Svirniuk <dmytro123svi@gmail.com>
2022-09-12 21:33:07 +0300

    Error correction

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,5 @@
 package org.example;
 
-
-import org.example.model.Contact;
-import org.example.model.ContactAge;
-import org.example.model.ContactNull;
-
 public class Main {
     public static void main(String[] args) {
 
